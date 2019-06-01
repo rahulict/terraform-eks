a1569746e589f44479b2732c8aec0d3032d48939
rahulict <32548256+rahulict@users.noreply.github.com>
2019-06-01 16:13:57 +0530

    Global tf

---
+++ global/s3/main.tf
@@ -0,0 +1,13 @@
+resource aws_s3_bucket tf-state-key {
+  bucket = "${var.tf_bucket}"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "${var.sse_algorithm}"
+      }
+    }
+  }
+}
--- global/s3/variables.tf
+++ global/s3/variables.tf
@@ -2,3 +2,11 @@ variable region {
   default     = "ap-south-1"
   description = "region to create aws resources"
 }
+variable tf_bucket {
+  default     = "tf-state-key"
+  description = "s3 bucket to store tf state files"
+}
+variable sse_algorithm {
+  default     = "AES256"
+  description = "server side encryption used by s3 bucket"
+}
