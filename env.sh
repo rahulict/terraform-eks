b0b631eff1345ba8ed52642fd333fcdebd9eab16
Rahul Kumar <rahul.kumar808@gmail.com>
2019-08-21 15:16:48 +0530

    New change~

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## awscli must be installed ##
 export TF_VAR_account_number=$(aws sts get-caller-identity --output text --query Account)
-export TF_VAR_region="ap-southeast-1"
+export TF_VAR_region="ap-northeast-1"
 export TF_VAR_tf_bucket="tf-state-key-${TF_VAR_account_number}-${TF_VAR_region}"
 #export TF_VAR_tf_bucket="tf-state-key-${TF_VAR_account_number}"
 export TF_VAR_tf_dynamodb_table="tf-lock-table-${TF_VAR_account_number}"
